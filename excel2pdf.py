7f68e6c8f1a4bea1caecee0c7754e4dd4d4b70dd
Christopher J Kansas <Christopher.Kansas@wu.com>
2017-09-28 11:54:48 -0600

    Fixed accessing the workbook; writing the cell on the threat sheet.

--- excel2pdf.py
+++ excel2pdf.py
@@ -10,13 +10,12 @@ country_codes = ["US, FR, AF"] #Need entire list from Claudine
 os.chdir(r"C:\Users\250250\Documents\Global Risk Assessment\Script Tests")
 
 #TODO open excel file and set variables for wb/sheets
-excel_file = input("Please enter the name of the Excel File:")
-work_book = openpyxl.load_workbook(os.path.abspath(excel_file)) #Need File Name - Could Prompt User to Enter File Name
+work_book = openpyxl.load_workbook('DRAFT TEST CERA 2017 - Threat Chart 2017.09.28.xlsm') #Need File Name - Could Prompt User to Enter File Name
 threat_sheet = work_book.get_sheet_by_name('Chart-Threat') #Need exact sheet names
 
 #TODO loop through the country codes - approx. 250 and set file to that code
 for code in country_codes:
-    threat_sheet[G4] = code #Need to confirm the sheet/cell that contains the country code
+    threat_sheet['G4'] = code #Need to confirm the sheet/cell that contains the country code
     work_book.save('DRAFT TEST CERA 2017 - %s - Threat Chart 2017.09.28.xlsm') % (code)
 #TODO within the loop file -> save as PDF (create files based on the country code)
 
